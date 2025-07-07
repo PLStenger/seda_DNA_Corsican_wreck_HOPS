20e2121f5fd4765a0b8b1346b91055b3ae8891d0
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2025-07-07 13:56:03 +0200

    Update 06_kraken_db.sh

--- 00_scripts/06_kraken_db.sh
+++ 00_scripts/06_kraken_db.sh
@@ -12,15 +12,35 @@ module load conda/4.12.0
 source ~/.bashrc
 conda activate kraken2
 
+
+# Aller sur https://ftp.ncbi.nlm.nih.gov/pub/taxonomy/accession2taxid/
+# et télécharger:
+# nucl_gb.accession2taxid.gz
+# nucl_wgs.accession2taxid.gz
+# pdb.accession2taxid.gz
+# dead_nucl.accession2taxid.gz
+# ex: wget https://ftp.ncbi.nlm.nih.gov/pub/taxonomy/accession2taxid/dead_nucl.accession2taxid.gz
+
+# gunzip nucl_gb.accession2taxid.gz
+# gunzip nucl_wgs.accession2taxid.gz
+# gunzip pdb.accession2taxid.gz
+# gunzip dead_nucl.accession2taxid.gz
+
+# mv nucl_gb.accession2taxid /home/plstenge/nt_kraken2_db/taxonomy/
+# mv nucl_wgs.accession2taxid /home/plstenge/nt_kraken2_db/taxonomy/
+# mv pdb.accession2taxid /home/plstenge/nt_kraken2_db/taxonomy/
+# mv dead_nucl.accession2taxid /home/plstenge/nt_kraken2_db/taxonomy/
+
+
 cd /home/plstenge/nt_kraken2_db
 
-kraken2-build --add-to-library /storage/biodatabanks/ncbi/NT/ncbi_blast_nt_2024-8-24/fasta/All/all.fasta --db /home/plstenge/nt_kraken2_db
+#kraken2-build --add-to-library /storage/biodatabanks/ncbi/NT/ncbi_blast_nt_2024-8-24/fasta/All/all.fasta --db /home/plstenge/nt_kraken2_db
 
-echo "add-to-library ok"
+#echo "add-to-library ok"
 
-kraken2-build --download-taxonomy --db /home/plstenge/nt_kraken2_db
+#kraken2-build --download-taxonomy --db /home/plstenge/nt_kraken2_db
 
-echo "download-taxonomy ok"
+#echo "download-taxonomy ok"
 
 kraken2-build --build --db /home/plstenge/nt_kraken2_db --threads 36
 
