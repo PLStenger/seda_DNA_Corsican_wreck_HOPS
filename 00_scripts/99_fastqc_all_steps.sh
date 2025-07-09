cf3af823404246bc3a5404965667ebe482e3b151
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2025-07-09 13:18:57 +0200

    Update 99_fastqc_all_steps.sh

--- 00_scripts/99_fastqc_all_steps.sh
+++ 00_scripts/99_fastqc_all_steps.sh
@@ -16,106 +16,106 @@
 
 # Correct tool citation : Andrews, S. (2010). FastQC: a quality control tool for high throughput sequence data.
 
-##############################################################################################################################
-####### Step 1: bbduk #######
-##############################################################################################################################
-
-WORKING_DIRECTORY=/home/plstenge/seda_DNA_Corsican_wreck_HOPS/03_bbduk
-OUTPUT=/home/plstenge/seda_DNA_Corsican_wreck_HOPS/99_02_quality_check_bbduk
-
-# Make the directory (mkdir) only if not existe already(-p)
-mkdir -p $OUTPUT
-
-module load conda/4.12.0
-source ~/.bashrc
-conda activate fastqc
-
-cd $WORKING_DIRECTORY
-
-for FILE in $(ls $WORKING_DIRECTORY/*.fastq.gz)
-do
-      fastqc $FILE -o $OUTPUT
-done ;
-
-conda deactivate
-
-module load conda/4.12.0
-source ~/.bashrc
-conda activate multiqc
-
-# Run multiqc for quality summary
-
-multiqc $OUTPUT
-
-conda deactivate
-
-
-##############################################################################################################################
-####### Step 2: fastuniq #######
-##############################################################################################################################
-
-WORKING_DIRECTORY=/home/plstenge/seda_DNA_Corsican_wreck_HOPS/04_fastuniq
-OUTPUT=/home/plstenge/seda_DNA_Corsican_wreck_HOPS/99_03_quality_check_fastuniq
-
-# Make the directory (mkdir) only if not existe already(-p)
-mkdir -p $OUTPUT
-
-module load conda/4.12.0
-source ~/.bashrc
-conda activate fastqc
-
-cd $WORKING_DIRECTORY
-
-for FILE in $(ls $WORKING_DIRECTORY/*.fastq.gz)
-do
-      fastqc $FILE -o $OUTPUT
-done ;
-
-conda deactivate
-
-module load conda/4.12.0
-source ~/.bashrc
-conda activate multiqc
-
-# Run multiqc for quality summary
-
-multiqc $OUTPUT
-
-conda deactivate
-
-
-##############################################################################################################################
-####### Step 3: clumpify #######
-##############################################################################################################################
-
-WORKING_DIRECTORY=/home/plstenge/seda_DNA_Corsican_wreck_HOPS/05_clumpify
-OUTPUT=/home/plstenge/seda_DNA_Corsican_wreck_HOPS/99_04_quality_check_clumpify
-
-# Make the directory (mkdir) only if not existe already(-p)
-mkdir -p $OUTPUT
-
-module load conda/4.12.0
-source ~/.bashrc
-conda activate fastqc
-
-cd $WORKING_DIRECTORY
-
-for FILE in $(ls $WORKING_DIRECTORY/*.fastq.gz)
-do
-      fastqc $FILE -o $OUTPUT
-done ;
-
-conda deactivate
-
-module load conda/4.12.0
-source ~/.bashrc
-conda activate multiqc
-
-# Run multiqc for quality summary
-
-multiqc $OUTPUT
-
-conda deactivate
+###############################################################################################################################
+######## Step 1: bbduk #######
+###############################################################################################################################
+#
+#WORKING_DIRECTORY=/home/plstenge/seda_DNA_Corsican_wreck_HOPS/03_bbduk
+#OUTPUT=/home/plstenge/seda_DNA_Corsican_wreck_HOPS/99_02_quality_check_bbduk
+#
+## Make the directory (mkdir) only if not existe already(-p)
+#mkdir -p $OUTPUT
+#
+#module load conda/4.12.0
+#source ~/.bashrc
+#conda activate fastqc
+#
+#cd $WORKING_DIRECTORY
+#
+#for FILE in $(ls $WORKING_DIRECTORY/*.fastq.gz)
+#do
+#      fastqc $FILE -o $OUTPUT
+#done ;
+#
+#conda deactivate
+#
+#module load conda/4.12.0
+#source ~/.bashrc
+#conda activate multiqc
+#
+## Run multiqc for quality summary
+#
+#multiqc $OUTPUT
+#
+#conda deactivate
+#
+#
+###############################################################################################################################
+######## Step 2: fastuniq #######
+###############################################################################################################################
+#
+#WORKING_DIRECTORY=/home/plstenge/seda_DNA_Corsican_wreck_HOPS/04_fastuniq
+#OUTPUT=/home/plstenge/seda_DNA_Corsican_wreck_HOPS/99_03_quality_check_fastuniq
+#
+## Make the directory (mkdir) only if not existe already(-p)
+#mkdir -p $OUTPUT
+#
+#module load conda/4.12.0
+#source ~/.bashrc
+#conda activate fastqc
+#
+#cd $WORKING_DIRECTORY
+#
+#for FILE in $(ls $WORKING_DIRECTORY/*.fastq.gz)
+#do
+#      fastqc $FILE -o $OUTPUT
+#done ;
+#
+#conda deactivate
+#
+#module load conda/4.12.0
+#source ~/.bashrc
+#conda activate multiqc
+#
+## Run multiqc for quality summary
+#
+#multiqc $OUTPUT
+#
+#conda deactivate
+#
+#
+###############################################################################################################################
+######## Step 3: clumpify #######
+###############################################################################################################################
+#
+#WORKING_DIRECTORY=/home/plstenge/seda_DNA_Corsican_wreck_HOPS/05_clumpify
+#OUTPUT=/home/plstenge/seda_DNA_Corsican_wreck_HOPS/99_04_quality_check_clumpify
+#
+## Make the directory (mkdir) only if not existe already(-p)
+#mkdir -p $OUTPUT
+#
+#module load conda/4.12.0
+#source ~/.bashrc
+#conda activate fastqc
+#
+#cd $WORKING_DIRECTORY
+#
+#for FILE in $(ls $WORKING_DIRECTORY/*.fastq.gz)
+#do
+#      fastqc $FILE -o $OUTPUT
+#done ;
+#
+#conda deactivate
+#
+#module load conda/4.12.0
+#source ~/.bashrc
+#conda activate multiqc
+#
+## Run multiqc for quality summary
+#
+#multiqc $OUTPUT
+#
+#conda deactivate
 
 ##############################################################################################################################
 ####### Step 4: fastp #######
