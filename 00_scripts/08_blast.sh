fd6cd2f95b6c6a1619ec563dfa0f4547b4e88d48
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2025-07-15 09:27:51 +0200

    Create 08_blast.sh

---
+++ 00_scripts/08_blast.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+#SBATCH --job-name=05_fastp
+#SBATCH --ntasks=1
+#SBATCH -p smp
+#SBATCH --mem=250G
+#SBATCH --mail-user=pierrelouis.stenger@gmail.com
+#SBATCH --mail-type=ALL 
+#SBATCH --error="/home/plstenge/seda_DNA_Corsican_wreck_HOPS/00_scripts/05_fastp.err"
+#SBATCH --output="/home/plstenge/seda_DNA_Corsican_wreck_HOPS/00_scripts/05_fastp.out"
+
+# Dossier contenant les fichiers fastq.gz
+INPUT_DIR="/home/plstenge/seda_DNA_Corsican_wreck_HOPS/06_fastp"
+OUTPUT_DIR="/home/plstenge/seda_DNA_Corsican_wreck_HOPS/08_blast"
+
+# Création des dossiers de sortie si besoin
+mkdir -p "$OUTPUT_DIR"
+mkdir -p "$LOG_DIR"
+
+module load conda/4.12.0
+source ~/.bashrc
+conda activate bbduk
+
+REFORMAT=/home/plstenge/bbmap/reformat.sh
+
+cd $INPUT_DIR
+
+gunzip clean_1120_sed6_rep3_dedup_clumpify_fastp_merged.fastq.gz 
+$REFORMAT in=$INPUT_DIR/clean_1120_sed6_rep3_dedup_clumpify_fastp_merged.fastq out=$OUTPUT_DIR/clean_1120_sed6_rep3_dedup_clumpify_fastp_merged.fa
+
+gunzip clean_1120_sed6_rep3_dedup_clumpify_fastp_R1.fastq.gz 
+$REFORMAT in=$INPUT_DIR/clean_1120_sed6_rep3_dedup_clumpify_fastp_R1.fastq out=$OUTPUT_DIR/clean_1120_sed6_rep3_dedup_clumpify_fastp_R1.fa
+
+gunzip clean_1120_sed6_rep3_dedup_clumpify_fastp_R2.fastq.gz 
+$REFORMAT in=$INPUT_DIR/clean_1120_sed6_rep3_dedup_clumpify_fastp_R2.fastq out=$OUTPUT_DIR/clean_1120_sed6_rep3_dedup_clumpify_fastp_R2.fa
+
+cd $OUTPUT_DIR
+
+blastn \
+  -query clean_1120_sed6_rep3_dedup_clumpify_fastp_merged.fa \
+  -db /storage/biodatabanks/ncbi/NT/current/flat/nt \
+  -out clean_1120_sed6_rep3_dedup_clumpify_fastp_merged.fa.tsv \
+  -outfmt 6 \
+  -max_target_seqs 10 \
+  -num_threads 4
+
+blastn \
+  -query clean_1120_sed6_rep3_dedup_clumpify_fastp_R1.fa \
+  -db /storage/biodatabanks/ncbi/NT/current/flat/nt \
+  -out clean_1120_sed6_rep3_dedup_clumpify_fastp_R1.fa.tsv \
+  -outfmt 6 \
+  -max_target_seqs 10 \
+  -num_threads 4
+
+blastn \
+  -query clean_1120_sed6_rep3_dedup_clumpify_fastp_R2.fa \
+  -db /storage/biodatabanks/ncbi/NT/current/flat/nt \
+  -out clean_1120_sed6_rep3_dedup_clumpify_fastp_R2.fa.tsv \
+  -outfmt 6 \
+  -max_target_seqs 10 \
+  -num_threads 4  
